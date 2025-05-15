109ad07d1d8bf1619bc3e182dcc9c59582920083
Ananthkrishhh <ananthkrishhh@gmail.com>
2025-05-15 12:53:41 +0530

    stock jsp files update

--- src/main/java/com/tradingapplication/TradingApplication/Controller/StockController.java
+++ src/main/java/com/tradingapplication/TradingApplication/Controller/StockController.java
@@ -4,11 +4,13 @@ import com.tradingapplication.TradingApplication.Service.StockServiceInterface;
 import com.tradingapplication.TradingApplication.dto.StockRequestDTO;
 import com.tradingapplication.TradingApplication.dto.StockResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/api/stocks")
 public class StockController {
 
@@ -24,4 +26,12 @@ public class StockController {
     public List<StockResponseDTO> getAllStocks() {
         return stockService.getAllStocks();
     }
+    
+    @GetMapping("/view")
+    public String viewStocks(Model model) {
+        List<StockResponseDTO> stocks = stockService.getAllStocks();
+        model.addAttribute("stocks", stocks);
+        return "stocks"; // maps to /WEB-INF/views/stocks.jsp
+    }
+    
 }
